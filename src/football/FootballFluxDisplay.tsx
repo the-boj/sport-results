01eecd99b759f49cfcbb4b1445351ade3fa83d6f
Hugo Beaujour <hugo.beaujour@mindflow.io>
2024-12-31 14:14:02 +0100

    Flux when no comments

--- src/football/FootballFluxDisplay.tsx
+++ src/football/FootballFluxDisplay.tsx
@@ -1,4 +1,4 @@
-import { FootballFlux } from '../types/football/flux';
+import { FootballFlux, Item } from '../types/football/flux';
 
 function backgroundColor(type: string) {
     if (type === 'but') {
@@ -17,6 +17,19 @@ function textColor(type: string) {
     return 'black';
 }
 
+function Remplacement({ item }: { item: Item }) {
+    if (item.objet.texte === 'remplace') {
+        return (
+            <div className='flex w-[85%] p-2'>
+                <div>{item.objet.attachment?.remplacement?.entrant.nom_abrege}</div>
+                <div className='ml-[5px] mr-[5px]'>{"->"}</div>
+                <div>{item.objet.attachment?.remplacement?.sortant.nom_abrege}</div>
+            </div>
+        );
+    }
+    return <div />;
+}
+
 interface Props {
     fluxData: FootballFlux;
 }
@@ -28,14 +41,18 @@ function FootballFluxDisplay({ fluxData }: Props) {
                 .filter((item) => item.objet.texte)
                 .map((item) => (
                     <div
-                        className="flex"
+                        className="flex border-b-2"
                         style={{
                             background: backgroundColor(item.objet.type),
                             color: textColor(item.objet.type),
                         }}
                     >
                         <div className="flex w-[15%] items-center justify-center">{item.objet.libelle_info}</div>
-                        <div className="w-[85%] border-b-2 p-2">{item.objet.texte}</div>
+                        {item.objet.texte === 'remplace' ? (
+                            <Remplacement item={item} />
+                        ) : (
+                            <div className="w-[85%] p-2">{item.objet.texte}</div>
+                        )}
                     </div>
                 ))}
         </div>
